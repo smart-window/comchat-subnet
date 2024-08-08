6081b8df0a88609b6bdcf21bea73529f57fd7fbf
root <root@31495.example.us>
2024-08-07 20:20:29 -0400

    increase max allowed uids

--- src/comchat/validator/_config.py
+++ src/comchat/validator/_config.py
@@ -4,5 +4,5 @@ from pydantic_settings import BaseSettings
 class ValidatorSettings(BaseSettings):
     # == Scoring ==
     iteration_interval: int = 4000  # Set, accordingly to your tempo.
-    max_allowed_weights: int = 800  # Query dynamically based on your subnet settings.
+    max_allowed_weights: int = 820  # Query dynamically based on your subnet settings.
     foo: int | None = None  # Anything else that you wish to implement.
